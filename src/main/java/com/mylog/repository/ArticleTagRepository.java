9ac82757e4e7e877166df2ea2f91c46eca2bd1e6
Jungho Kim <jeonghogim318@gmail.com>
2025-03-18 10:00:00 +0900

    fix: 복합키 Id 연결 수정

--- src/main/java/com/mylog/repository/ArticleTagRepository.java
+++ src/main/java/com/mylog/repository/ArticleTagRepository.java
@@ -1,10 +1,11 @@
 package com.mylog.repository;
 
 import com.mylog.entity.ArticleTag;
+import com.mylog.entity.compositekey.ArticleTagId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleTagRepository extends JpaRepository<ArticleTag, Long> {
+public interface ArticleTagRepository extends JpaRepository<ArticleTag, ArticleTagId> {
 
 }
