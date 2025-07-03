5174396ed7eafae444672673f214a1bdae8330a0
Jungho Kim <jeonghogim318@gmail.com>
2025-07-03 09:50:00 +0900

    refactor: CustomUserDetailsService 읽기 서비스로 명시

--- src/main/java/com/mylog/service/CustomUserDetailsReadService.java
+++ src/main/java/com/mylog/service/CustomUserDetailsReadService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsReadService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
