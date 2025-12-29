b83420fba0a25f25615d25c211ce478d26ab344e
Jungho Kim <jeonghogim318@gmail.com>
2025-12-29 17:44:14 +0900

    refactor: 모듈화 설정 완료
    - 폴더분리만 완료 => 코드이동 및 분리 필요

--- src/main/java/com/mylog/common/response/CommonValue.java
+++
@@ -1,8 +0,0 @@
-package com.mylog.common.response;
-
-public class CommonValue {
-
-    //카테고리
-    public static final int CATEGORY_LIMIT = 20;
-    public static final String ORIGIN_CATEGORY = "없음";
-}
