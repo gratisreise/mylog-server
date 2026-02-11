93cf619a55e36b5b953fc902f18aa7547c7fe30f
Jungho Kim <jeonghogim318@gmail.com>
2026-02-12 08:07:36 +0900

    refactor(common): 예외 처리 및 응답 구조 통합
    - 개별 예외 클래스(C*Exception)를 BusinessException으로 통합
    - ResultCode enum을 ErrorCode enum으로 대체
    - ResponseService 기반 응답을 BaseResponse/ErrorResponse/SuccessResponse 구조로 변경
    - GlobalExceptionHandler에서 ResponseEntity 기반 응답 처리 방식 적용
    - 유효성 검증 및 타입 불일치 예외 처리 로직 추가

--- src/main/java/com/mylog/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mylog/common/exception/GlobalExceptionHandler.java
@@ -1,51 +1,66 @@
 package com.mylog.common.exception;
 
 
-import com.mylog.common.response.CommonResult;
-import com.mylog.common.response.ResponseService;
+import com.mylog.common.response.ErrorResponse;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CDuplicatedException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResult handleDuplicatedException(CDuplicatedException ex){
-        return ResponseService.getFailResult(ResultCode.DATA_DUPLICATED);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String message = getErrorMessage(ex);
+        String code = ErrorCode.VALIDATION_FAILED.getCode();
+        return ResponseEntity
+            .status(ex.getStatusCode())
+            .body(ErrorResponse.from(code, message));
     }
 
-    @ExceptionHandler(CMissingDataException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResult handleMissingDataException(CMissingDataException ex){
-        return ResponseService.getFailResult(ResultCode.DATA_MISSED);
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        String message = getErrorMessage(ex);
+        String code = ErrorCode.TYPE_MISMATCH.getCode();
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse.from(code, message));
     }
 
-    @ExceptionHandler(CInvalidDataException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResult handleInvalidDataException(CInvalidDataException ex){
-        return ResponseService.getFailResult(ResultCode.DATA_INVALID);
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> businessExceptionHandler(BusinessException ex) {
+        ErrorCode errorCode = ex.getCode();
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.from(errorCode));
     }
 
-    @ExceptionHandler(CUnAuthorizedException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public CommonResult handleUnAuthorizedException(CUnAuthorizedException ex){
-        return ResponseService.getFailResult(ResultCode.UNAUTHORIZED_ACCESS);
+    // 처리되지 않은 오류
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> missingExceptionHandler(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.unknown(ex));
     }
 
-    @ExceptionHandler(RestClientException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResult handleRestClientException(RestClientException ex){
-        return ResponseService.getFailResult(ex);
+    private static @NonNull String getErrorMessage(MethodArgumentTypeMismatchException ex) {
+        return String.format("'%s'의 타입이 맞지 않습니다.", ex.getName());
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResult handleRuntimeException(RuntimeException ex){
-        return ResponseService.getFailResult(ex);
+    private static String getErrorMessage(MethodArgumentNotValidException ex) {
+        String where = ex.getBindingResult().getFieldErrors().get(0).getField();
+
+        String how = ex.getBindingResult()
+            .getFieldErrors()
+            .get(0)
+            .getDefaultMessage();
+
+        return String.format("'%s' 검증실패,  %s.", where, how);
     }
 
 }
