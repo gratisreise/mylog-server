d56fe27761a484efc066c86f94ef2b0bc431c0d8
Jungho Kim <jeonghogim318@gmail.com>
2026-02-11 19:35:12 +0900

    refactor(external): 외부 서비스 패키지 구조 재편성
    - redis: RedisConfig 패키지 이동 및 RedisService 추가
    - s3: S3Config, S3Service 패키지 이동

---
+++ src/main/java/com/mylog/external/redis/RedisService.java
@@ -0,0 +1,5 @@
+package com.mylog.external.redis;
+
+public class RedisService {
+
+}
--- src/main/java/com/mylog/external/s3/S3Config.java
+++ src/main/java/com/mylog/external/s3/S3Config.java
@@ -1,4 +1,4 @@
-package com.mylog.config;
+package com.mylog.external.s3;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
