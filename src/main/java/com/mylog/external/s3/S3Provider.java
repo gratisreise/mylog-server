a9584c1c0f5f65ebec48178e6367a19c7c5fa1c2
Jungho Kim <jeonghogim318@gmail.com>
2026-02-12 08:54:58 +0900

    refactor(s3): S3Service를 S3Provider로 이름 변경 및 코드 정리
    - S3Service를 S3Provider로 클래스명 변경
    - @Service를 @Component로 변경
    - 불필요한 @Slf4j 및 로그 제거
    - S3Config credentials 생성 방식 정리

--- src/main/java/com/mylog/external/s3/S3Provider.java
+++ src/main/java/com/mylog/external/s3/S3Provider.java
@@ -1,22 +1,20 @@
 package com.mylog.external.s3;
 
-import com.mylog.common.exception.CMissingDataException;
 import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-@Service
+@Component
 @RequiredArgsConstructor
-@Slf4j
-public class S3Service {
+public class S3Provider {
+
     private final S3Client s3Client;
 
     @Value("${cloud.aws.s3.bucket}")
@@ -34,23 +32,18 @@ public class S3Service {
             .contentType(contentType)
             .build();
 
-        try{
-            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(
-                file.getInputStream(), file.getSize()
-            ));
-        } catch (RuntimeException e) {
-            throw new CMissingDataException("s3 이미지 업로드에 실패했습니다.");
-        }
 
-        String result = String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
-        log.info("s3 result: {}", result);
-        return result;
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(
+            file.getInputStream(), file.getSize()
+        ));
+
+
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
     }
 
     // 이미지 삭제
     public void deleteImage(String url) {
         String fileKey = url.substring(url.lastIndexOf("/") + 1);
-        log.info("fileKey: {}", fileKey);
         DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
             .bucket(bucketName)
             .key(fileKey)
